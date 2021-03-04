2205ec5701d5a5a5c196333cd0356969f42dff82
Ekta246 <ebhojwani246@gmail.com>
2021-03-04 00:45:01 -0500

    extracting annotations from labelbox

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,12 +1,13 @@
-'''
+#Author : Ekta Bhojwani
+#MSECE
+'''A script to convert labelbox annotations to particular form of annotations for training phase'''
 import json
 import cv2
 import numpy as np
 import os
 #data_dir='./hom'
 #data_catal={0:,1:,}
-json_file_path='/home/ekta/Downloads/export-2020-11-26T08_00_02.265Z.json'
-#image_root_path='/home/ekta/Documents/vdot/inlets_folder/New Images_Patrick'
+json_file_path='/export-2020-11-26T08_00_02.265Z.json'
 val_list=['202010221219370356.jpg','202010221221200914.jpg','202010221221350739.jpg' ,'202010221255500592.jpg','202010221256290726.jpg','202010221257410790.jpg','202010271410380820.jpg','202010271419260978.jpg','202010271419530136.jpg','202010271421500108.jpg',
 '202010271422060801.jpg', '202010271436340604.jpg', '202010271436510210.jpg', '202010271459040930.jpg', '202010271459230349.jpg','202010291052590698.jpg','202010291053190890.jpg' , '202010291059020914.jpg','202010291105010345.jpg','202011110657510322.jpg','202011110658010100.jpg','202011110658500474.jpg','202011110659480957.jpg','202011110700040338.jpg','202011110701020011.jpg','202011110701150944.jpg', '202011110702210830.jpg','202011110703380836.jpg','202011110703540298.jpg','202011110704580228.jpg','202011110705090039.jpg','202011110706340827.jpg']
 
@@ -43,18 +44,18 @@ for ann in label_dict:
     #break
 #print(annotations)
 with open('unseen.json', 'w') as fp:
-    json.dump(annotations, fp)'''
+    json.dump(annotations, fp)
 
 
 
-
+'''A script to make custom annotations for test set '''
 
 
 import json
 import os
 import numpy as np
 
-image_path='/home/ekta/AI_current/vdot/vdot/Just_400/others_set'
+image_path='AI_current/vdot/vdot/Just_400/others_set'
 val_list=os.listdir(image_path)
 annotations={}
 #annotation={}
